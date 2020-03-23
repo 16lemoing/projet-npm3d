220647b6c3d34fca2ae234f62fb41dafb9b7fc5e
Hugues Souchard de Lavoreille <hugues.sdl@gmail.com>
2020-03-24 00:02:43 +0100

    Added s-voxels

--- bin/main.py
+++ bin/main.py
@@ -1,25 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+sys.path.append("../src")
 import time
 import numpy as np
 from sklearn.neighbors import KDTree
 from utils.ply import read_ply, write_ply
 from plots import show_points
+from descriptors import local_PCA
 
-# Generating a smaller dataset for tests
-# data = read_ply("../data/GT_Madame1_2.ply")
-# cloud = np.vstack((data['x'], data['y'], data['z'])).T
-# dlaser = data['reflectance']
-# dlabel = data["label"]
-# dclass = data["class"]
-# write_ply('../data/Madame_extremelysmall.ply', [cloud[:50000], dlaser[:50000].astype(int), dlabel[:50000].astype(int), dclass[:50000].astype(int)],['x', 'y', 'z', 'reflectance', 'label', 'class'])
 
-data = read_ply("../data/Madame_extremelysmall.ply")
-cloud = np.vstack((data['x'], data['y'], data['z'])).T
-dlaser = data['reflectance']
-dlabel = data["label"]
-dclass = data["class"]
+
+# The functions currently in this file for testing should be moved later into the source directory 
 
 def determine_bounding_box(points):
     return np.min(points, axis=0), np.max(points, axis=0)
@@ -34,6 +27,7 @@ def voxelize_cloud(cloud, max_voxel_size = 0.3, seed = 42):
     available_idxs_mask = np.array(len(cloud) * [True])
     
     voxels = []
+    voxels_mask = []
     
     while len(available_idxs) > 0:
         print(len(available_idxs))
@@ -58,7 +52,51 @@ def voxelize_cloud(cloud, max_voxel_size = 0.3, seed = 42):
         
         # Storing into voxel
         voxels.append(voxel_idxs)
+        voxels_mask.append(voxel_idxs_mask)
+    
+    return voxels, np.array(voxels_mask)
+
+def compute_voxel_features(cloud, dlaser, voxels):
+    
+    nb_voxels = len(voxels)
+    
+    # TODO C'est quoi le "centre géométrique" ? Ici ma supposition
+    voxel_geometric_center = np.zeros((nb_voxels, 3))
+    voxel_mean_intensity = np.zeros(nb_voxels)
+    voxel_var_intensity = np.zeros(nb_voxels)
+    voxel_size = np.zeros((nb_voxels, 3))
+    voxel_normal = np.zeros((nb_voxels, 3))
+    
+    for i in range(len(voxels)):
+        vmin, vmax = determine_bounding_box(cloud[voxels[i], :])
+        voxel_geometric_center[i] = (vmin + vmax) / 2
+        voxel_mean_intensity[i] = dlaser[voxels[i]].mean()
+        voxel_var_intensity[i] = dlaser[voxels[i]].var()
+        voxel_size[i] = vmax - vmin
+        eigval, eigvec = local_PCA(cloud[voxels[i], :])
+        voxel_normal[i] = eigvec[:, 2]
         
-    show_points(cloud, voxels)
+    return voxel_geometric_center, voxel_mean_intensity, voxel_var_intensity, voxel_size, voxel_normal
+        
+    
+    
+    
+# Generating a smaller dataset for tests
+# data = read_ply("../data/GT_Madame1_2.ply")
+# cloud = np.vstack((data['x'], data['y'], data['z'])).T
+# dlaser = data['reflectance']
+# dlabel = data["label"]
+# dclass = data["class"]
+# write_ply('../data/Madame_extremelysmall.ply', [cloud[:50000], dlaser[:50000].astype(int), dlabel[:50000].astype(int), dclass[:50000].astype(int)],['x', 'y', 'z', 'reflectance', 'label', 'class'])
+
+data = read_ply("../data/Madame_extremelysmall.ply")
+cloud = np.vstack((data['x'], data['y'], data['z'])).T
+dlaser = data['reflectance']
+dlabel = data["label"]
+dclass = data["class"]
+
+voxels, voxels_mask = voxelize_cloud(cloud)
+v_gc, v_mi, v_vi, v_s, v_n = compute_voxel_features(cloud, dlaser, voxels)
+show_points(cloud, voxels, v_mi)
         
     
--- src/plots.py
+++ src/plots.py
@@ -3,15 +3,33 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def show_points(cloud, idxs):
+def show_points(cloud, idxs, color=None):
+    """
+    3D scatter plot of points, grouped by voxel 
+
+    Parameters
+    ----------
+    cloud : Nx3 cloud of all points
+    idxs : list of indexes, of size n_voxels. Each element is a list of the 
+           indices of the points of 'clouds' which belong to the corresponding voxel
+    color : numpy array of floats of size n_voxels, optional
+        If provided, it is used to displauy each voxel with a specific color
+    """
+    
+    if color is not None:
+        color = cm.jet(color / np.max(color))
+    
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    for i in idxs:
-        data = cloud[i, :]
-        ax.plot(data[:,0], data[:,1], data[:,2], '.')
+    for i in range(len(idxs)):
+        data = cloud[idxs[i], :]
+        ax.plot(data[:,0], data[:,1], data[:,2], '.', **({'c': color[i]} if color is not None else {}))
+    
     plt.show()
+    
 
 
