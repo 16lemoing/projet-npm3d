c6cd5cc3abae38f72d4efb72eeec765cceed70fc
Hugues Souchard de Lavoreille <hugues.sdl@gmail.com>
2020-03-28 17:07:38 +0100

    Cleaned code, added component class

--- src/plots.py
+++ src/plots.py
@@ -5,31 +5,50 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
-
-
-def show_points(cloud, idxs, color=None):
+    
+def plot_voxels(voxelcloud, idxs = None, colors = None, only_center = False):
     """
-    3D scatter plot of points, grouped by voxel 
-
-    Parameters
-    ----------
-    cloud : Nx3 cloud of all points
-    idxs : list of indexes, of size n_voxels. Each element is a list of the 
-           indices of the points of 'clouds' which belong to the corresponding voxel
-    color : numpy array of floats of size n_voxels, optional
-        If provided, it is used to displauy each voxel with a specific color
+    Plots some voxels from a voxel cloud
     """
     
-    if color is not None:
-        color = cm.jet(color / np.max(color))
+    if idxs is None:
+        idxs = list(range(len(voxelcloud)))
+    
+    if type(idxs) is int:
+        idxs = [idxs]
+        
+    if colors is not None:
+        if len(colors) != len(idxs):
+            raise Exception("Colors must have the same shape as idxs")
+        colors = cm.jet(colors / np.max(colors))
     
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     for i in range(len(idxs)):
-        data = cloud.points[idxs[i], :]
-        ax.plot(data[:,0], data[:,1], data[:,2], '.', **({'c': color[i]} if color is not None else {}))
+        if only_center:
+            voxelcloud.plot_voxel_geometric_center(ax, idxs[i], **({'c': colors[i]} if colors is not None else {}))
+        else:
+            voxelcloud.plot_voxel_points(ax, idxs[i], **({'c': colors[i]} if colors is not None else {}))
     
     plt.show()
     
-
+    
+def plot_components(list_of_components, colors = None, only_voxel_center = True):
+    """
+    Plots some components from a list of components objects
+    """
+    
+    if colors is not None:
+        if len(colors) != len(list_of_components):
+            raise Exception("Colors must have the same shape as the list of components")
+        colors = cm.jet(colors / np.max(colors))
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for c in list_of_components:
+        if only_voxel_center:
+            c.plot_component_voxel_centers(ax, **({'c': colors[i]} if colors is not None else {}))
+        else:
+            c.plot_component_points(ax, **({'c': colors[i]} if colors is not None else {}))
+    plt.show()
 
